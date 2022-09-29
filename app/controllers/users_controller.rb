d34959ccc24c20e68775d556a575a2788599ff9d
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-09-29 15:48:49 +0100

    Add signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the General Superintendent Book Distribution Platform #{@user.username} group, you have successfully signed up"
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
